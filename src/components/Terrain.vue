f9f89b7948981ed6447e11979d0d4b6d20abd893
Ami Surakka <ami@varjohovi.net>
2021-03-03 00:40:38 +0200

    Smoothed out map animations.

--- src/components/Terrain.vue
+++ src/components/Terrain.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import {debounce} from 'lodash'
+import {debounce, throttle} from 'lodash'
 import Tile from "@/components/Tile";
 import {mapState} from "vuex";
 
@@ -16,17 +16,26 @@ export default {
   name: "Terrain",
   components: {Tile},
 
+  data() {
+    return {
+      loader: null
+    }
+  },
+
   created() {
     this.$store.commit('addTile', {x: 0, y: 0, terrain: null})
   },
 
   beforeUpdate() {
-    debounce(() => {
+    this.loader = debounce(() => {
       this.$store.commit('startWorking')
-    }, 500)
+    }, 100)
+
+    this.loader()
   },
 
   updated() {
+    this.loader.cancel()
     this.$store.commit('stopWorking')
   },
 
@@ -40,27 +49,29 @@ export default {
     },
 
     pane($event) {
-      const scrollBy = 100
-      const scroll = {
-        top: this.$el.scrollTop,
-        left: this.$el.scrollLeft,
-        behavior: 'smooth'
-      }
+      throttle(() => {
+        const scrollBy = 100
+        const scroll = {
+          top: this.$el.scrollTop,
+          left: this.$el.scrollLeft,
+          behavior: 'smooth'
+        }
 
-      switch ($event.key) {
-        case 'w':
-          this.$el.scroll({...scroll, top: scroll.top - scrollBy})
-          break
-        case 's':
-          this.$el.scroll({...scroll, top: scroll.top + scrollBy})
-          break
-        case 'd':
-          this.$el.scroll({...scroll, left: scroll.left + scrollBy})
-          break
-        case 'a':
-          this.$el.scroll({...scroll, left: scroll.left - scrollBy})
-          break
-      }
+        switch ($event.key) {
+          case 'w':
+            this.$el.scroll({...scroll, top: scroll.top - scrollBy})
+            break
+          case 's':
+            this.$el.scroll({...scroll, top: scroll.top + scrollBy})
+            break
+          case 'd':
+            this.$el.scroll({...scroll, left: scroll.left + scrollBy})
+            break
+          case 'a':
+            this.$el.scroll({...scroll, left: scroll.left - scrollBy})
+            break
+        }
+      }, 200)()
     }
   },
 
