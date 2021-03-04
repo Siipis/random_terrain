73bb32ac437c4eea8bcbb9e403ca0c6175d77c02
Ami Surakka <ami@varjohovi.net>
2021-03-04 23:06:34 +0200

    Refactored smart positioning from Terrain.vue to App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" @keypress.prevent="pane">
     <transition name="fade">
       <div id="loading" v-if="working">
         <div class="icon">
@@ -9,13 +9,14 @@
     </transition>
 
     <Settings/>
-    <Terrain/>
+    <Terrain ref="terrain"/>
   </div>
 </template>
 
 <script>
 import 'vue-material-design-icons/styles.css';
 
+import {throttle} from "lodash";
 import Terrain from "@/components/Terrain";
 import Settings from "@/components/Settings";
 import LoadingIcon from 'vue-material-design-icons/Refresh'
@@ -28,9 +29,114 @@ export default {
     LoadingIcon,
   },
 
+  data() {
+    return {
+      position: {}
+    }
+  },
+
+  mounted() {
+    this.container.focus()
+
+    this.resetScrollPosition()
+    this.saveScrollPosition()
+  },
+
+  beforeUpdate() {
+    this.saveScrollPosition()
+  },
+
+  updated() {
+    this.recoverScrollPosition()
+  },
+
+  methods: {
+    pane($event) {
+      throttle(() => {
+        const scrollBy = 100
+
+        let scrollTopBy = 0
+        let scrollLeftBy = 0
+
+        switch ($event.key) {
+          case 'w':
+            scrollTopBy = -scrollBy
+            break
+          case 'a':
+            scrollLeftBy = -scrollBy
+            break
+          case 's':
+            scrollTopBy = scrollBy
+            break
+          case 'd':
+            scrollLeftBy = scrollBy
+            break
+        }
+
+        this.container.scrollBy({
+          top: scrollTopBy,
+          left: scrollLeftBy,
+          behavior: 'smooth'
+        })
+      }, 200)()
+    },
+
+    saveScrollPosition() {
+      const {scrollHeight, scrollWidth, clientHeight, clientWidth, scrollTop, scrollLeft} = this.container
+
+      this.position = {
+        scrollHeight, scrollWidth,
+        isScrolledTop: scrollTop === 0,
+        isScrolledBottom: scrollTop + clientHeight === scrollHeight,
+        isScrolledLeft: scrollLeft === 0,
+        isScrolledRight: scrollLeft + clientWidth === scrollWidth,
+      }
+    },
+
+    recoverScrollPosition() {
+      const {scrollHeight, scrollWidth} = this.container
+
+      let scrollTopBy = 0
+      if (this.position.isScrolledTop) {
+        scrollTopBy = -scrollHeight
+      } else if (this.position.isScrolledBottom) {
+        scrollTopBy = scrollHeight
+      } else {
+        scrollTopBy = (scrollHeight - this.position.scrollHeight) / 2
+      }
+
+      let scrollLeftBy = 0
+      if (this.position.isScrolledLeft) {
+        scrollLeftBy = -scrollWidth
+      } else if (this.position.isScrolledRight) {
+        scrollLeftBy = scrollWidth
+      } else {
+        scrollLeftBy = (scrollWidth - this.position.scrollWidth) / 2
+      }
+
+      this.container.scrollBy({
+        top: scrollTopBy,
+        left: scrollLeftBy
+      })
+    },
+
+    resetScrollPosition() {
+      const {scrollHeight, scrollWidth, clientHeight, clientWidth} = this.container
+
+      this.container.scroll({
+        top: (scrollHeight - clientHeight) / 2,
+        left: (scrollWidth - clientWidth) / 2
+      })
+    }
+  },
+
   computed: {
     working() {
       return this.$store.state.working
+    },
+
+    container() {
+      return this.$refs.terrain.$el
     }
   }
 }
--- src/components/Terrain.vue
+++ src/components/Terrain.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="terrain" tabindex="1"
-       @wheel.prevent="zoom" @keypress.prevent="pane">
+  <div id="terrain" tabindex="1" @wheel.prevent="zoom">
     <div id="terrain--container" ref="container" :style="containerStyle">
       <transition-group name="terrain" tag="div" id="tile--grid" :style="gridStyle">
         <Tile v-for="tile in tiles" :tile="tile" :key="tile.key"/>
@@ -10,7 +9,7 @@
 </template>
 
 <script>
-import {debounce, throttle, delay} from 'lodash'
+import {debounce, delay} from 'lodash'
 import Tile from "@/components/Tile";
 import {mapState, mapGetters} from "vuex";
 
@@ -22,22 +21,22 @@ export default {
     return {
       showLoader: null,
       animate: false,
-      position: {}
+
+      container: {
+        clientHeight: 0,
+        clientWidth: 0
+      }
     }
   },
 
   mounted() {
-    this.resetScrollPosition()
-    this.saveScrollPosition()
-
+    this.syncWithContainer()
     delay(() => {
       this.animate = true
     }, 1000)
   },
 
   beforeUpdate() {
-    this.saveScrollPosition()
-
     this.showLoader = debounce(() => {
       this.$store.commit('startWorking')
     }, 100)
@@ -46,7 +45,6 @@ export default {
   },
 
   updated() {
-    this.recoverScrollPosition()
     this.animate = true
 
     this.showLoader.cancel()
@@ -62,88 +60,20 @@ export default {
       }
     },
 
-    pane($event) {
-      throttle(() => {
-        const scrollBy = 100
-        const scroll = {
-          top: this.$el.scrollTop,
-          left: this.$el.scrollLeft,
-          behavior: 'smooth'
-        }
+    syncWithContainer() {
+      const {clientHeight, clientWidth} = this.$el
 
-        switch ($event.key) {
-          case 'w':
-            this.$el.scroll({...scroll, top: scroll.top - scrollBy})
-            break
-          case 's':
-            this.$el.scroll({...scroll, top: scroll.top + scrollBy})
-            break
-          case 'd':
-            this.$el.scroll({...scroll, left: scroll.left + scrollBy})
-            break
-          case 'a':
-            this.$el.scroll({...scroll, left: scroll.left - scrollBy})
-            break
-        }
-      }, 200)()
-    },
-
-    saveScrollPosition() {
-      const {scrollHeight, scrollWidth, clientHeight, clientWidth, scrollTop, scrollLeft} = this.$el
-
-      this.position = {
-        scrollHeight, scrollWidth,
-        clientHeight, clientWidth,
-
-        isScrolledTop: scrollTop === 0,
-        isScrolledBottom: scrollTop + clientHeight === scrollHeight,
-        isScrolledLeft: scrollLeft === 0,
-        isScrolledRight: scrollLeft + clientWidth === scrollWidth,
+      this.container = {
+        clientHeight, clientWidth
       }
     },
-
-    recoverScrollPosition() {
-      const {scrollHeight, scrollWidth} = this.$el
-
-      let scrollTopBy = 0
-      if (this.position.isScrolledTop) {
-        scrollTopBy = -scrollHeight
-      } else if (this.position.isScrolledBottom) {
-        scrollTopBy = scrollHeight
-      } else {
-        scrollTopBy = (scrollHeight - this.position.scrollHeight) / 2
-      }
-
-      let scrollLeftBy = 0
-      if (this.position.isScrolledLeft) {
-        scrollLeftBy = -scrollWidth
-      } else if (this.position.isScrolledRight) {
-        scrollLeftBy = scrollWidth
-      } else {
-        scrollLeftBy = (scrollWidth - this.position.scrollWidth) / 2
-      }
-
-      this.$el.scrollBy({
-        top: scrollTopBy,
-        left: scrollLeftBy
-      })
-    },
-
-    resetScrollPosition() {
-      const {scrollHeight, scrollWidth, clientHeight, clientWidth} = this.$el
-
-      this.$el.scroll({
-        top: (scrollHeight - clientHeight) / 2,
-        left: (scrollWidth - clientWidth) / 2
-      })
-    }
   },
 
   computed: {
     containerStyle() {
       const tileSize = this.scale * 100
-      const paddingY = Math.max((this.position.clientHeight - this.size.height * tileSize) / 2, 0)
-      const paddingX = Math.max((this.position.clientWidth - this.size.width * tileSize) / 2, 0)
+      const paddingY = Math.max((this.container.clientHeight - this.size.height * tileSize) / 2, 0)
+      const paddingX = Math.max((this.container.clientWidth - this.size.width * tileSize) / 2, 0)
 
       return {
         padding: `${Math.floor(paddingY)}px ${Math.floor(paddingX)}px`,
@@ -172,6 +102,7 @@ export default {
 
   watch: {
     tiles() {
+      this.syncWithContainer()
       this.animate = false
     }
   }
